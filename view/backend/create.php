a1948d1ca354600a5e4986bb9c38153ca84f5319
farulwananda <doctorbranz@gmail.com>
2023-01-07 01:40:41 +0700

    feat: create pasien
    sweetalert not working

--- view/backend/create.php
+++ view/backend/create.php
@@ -1,17 +1,45 @@
 <?php
-require 'connect.php';
+require '../config/connect.php';
 
-$nama = $_POST['nama'];
-$alamat = $_POST['alamat'];
-$nomor_hp = $_POST['nomor_telepon'];
-$ttl = $_POST['ttl'];
-$gender = $_POST['gender'];
+if (isset($_POST['insert-pasien'])) {
+    $nama = htmlspecialchars(trim($_POST['nama']));
+    $alamat = htmlspecialchars(trim($_POST['alamat']));
+    $nomor_hp = htmlspecialchars(trim($_POST['nomor_telepon']));
+    $ttl = htmlspecialchars(trim($_POST['tanggal_lahir']));
+    $gender = htmlspecialchars(trim($_POST['jenis_kelamin']));
 
-$query = "INSERT INTO pasien(nama, tanggal_lahir, jenis_kelamin, nomor_telepon, alamat) VALUES ('$nama', '$ttl', '$gender', '$nomor_hp', '$alamat')";
-
-if(mysqli_query($conn, $query)){
-    // echo "<script>alert('Data berhasil ditambahkan!');</script>";
-    header('location: ../view/pasien.php');
-} else {
-    echo "Error : ". $query . "<br>" . mysqli_error($conn);
+    if (empty($nama)) {
+        echo "
+        <script>
+        setTimeout(function() {
+            Swal.fire({
+                title: 'Peringatan!',
+                text: 'isi yang bener dong!',
+                icon: 'error',
+                timer: 2000,
+                showCancelButton: false,
+                showConfirmButton: false
+            });
+        });  
+        </script>
+        ";
+    } else {
+        mysqli_query($conn, "INSERT INTO pasien (nama, tanggal_lahir, jenis_kelamin, nomor_telepon, alamat) VALUES ('$nama', '$ttl', '$gender', '$nomor_hp', '$alamat')");
+        echo "
+        <script>
+        setTimeout(function() {
+            Swal.fire({
+                title: 'Berhasil!',
+                text: 'yey berhasil!',
+                icon: 'success',
+                showCancelButton: false,
+                showConfirmButton: false
+            });
+        });  
+        window.setTimeout(function(){ 
+            window.location.replace('pasien.php');
+        },2000);
+        </script>
+        ";
+    }
 }
--- view/pasien.php
+++ view/pasien.php
@@ -3,7 +3,8 @@
 <?php include 'layouts/navbar.php'; ?>
 
 <main class="w-full max-screen-xl max-w-6xl mt-4 mb-4 mx-auto p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-  <form action="../backend/create.php" method="POST">
+  <?php require 'backend/create.php'; ?>
+  <form action="" method="POST">
     <div class="mb-6">
       <label for="nama_lengkap" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Lengkap</label>
       <input type="name" name="nama" id="nama_lengkap" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Jhon Doe" required />
@@ -15,15 +16,15 @@
     <div class="grid gap-3 mb-6 md:grid-cols-3">
       <div>
         <label for="nomor_telepon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor Telepon</label>
-        <input type="number" name="nomor_telepon" id="nomor_telepon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+        <input type="text" name="nomor_telepon" id="nomor_telepon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" required />
       </div>
       <div>
         <label for="birth_date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal Lahir</label>
-        <input type="date" name="ttl" id="birth_date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+        <input type="date" name="tanggal_lahir" id="birth_date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
       </div>
       <div>
         <label for="gender" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Kelamin</label>
-        <select name="gender" id="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        <select name="jenis_kelamin" id="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
           <option disabled selected value>Pilih Jenis Kelamin</option>
           <option value="Laki-Laki">Laki-Laki</option>
           <option value="Perempuan">Perempuan</option>
@@ -31,7 +32,7 @@
       </div>
     </div>
 
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <button type="submit" name="insert-pasien" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
       Submit
     </button>
 
